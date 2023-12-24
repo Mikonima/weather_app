39150f5fdd8e569d592639382f6629dec66f8cee
Maksim <maksim.rukosuev@bk.ru>
2023-12-25 04:51:23 +0700

    Добавлен Интерфейс

--- app/src/main/java/com/example/weather_app/fragments/MainFragment.kt
+++ app/src/main/java/com/example/weather_app/fragments/MainFragment.kt
@@ -8,10 +8,22 @@ import android.widget.Toast
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.viewpager2.widget.ViewPager2
+import com.example.weather_app.adapters.VpAdapte
 import com.example.weather_app.databinding.FragmentMainBinding
+import com.google.android.material.tabs.TabLayoutMediator
 
 //главный фрагмент
 class MainFragment : Fragment() {
+    private val flist= listOf(
+        HoursFragment.newInstance(),
+        DaysFragment.newInstance()
+    )
+    private val tList= listOf(
+        "HOURS",
+        "DAYS"
+    )
     private lateinit var pLauncher:ActivityResultLauncher<String>
     private lateinit var binding: FragmentMainBinding
 
@@ -25,13 +37,25 @@ class MainFragment : Fragment() {
 
         return binding.root
     }
+
 // проверка(запускается) разрешение на местоположение
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         permissionListener()
         checkPermission()
+        init()
 
     }
+    //переключение табло
+    private fun init()=with(binding){
+        val adapter=VpAdapte(activity as FragmentActivity,flist)
+        vp.adapter=adapter
+        TabLayoutMediator(tabLayout,vp)
+        {
+            tab,pos->tab.text=tList[pos]
+        }.attach()
+    }
+
 //даёт ли пользователь разрешение на исп. местоположения
     private fun permissionListener(){
         pLauncher=registerForActivityResult(
