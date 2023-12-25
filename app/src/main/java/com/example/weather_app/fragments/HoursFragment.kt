ae84b9ee8326469ba60c7d5e35b0fdd21936ad1c
Maksim <maksim.rukosuev@bk.ru>
2023-12-25 09:03:02 +0700

    Интерфейс отображает данные

--- app/src/main/java/com/example/weather_app/fragments/HoursFragment.kt
+++ app/src/main/java/com/example/weather_app/fragments/HoursFragment.kt
@@ -5,19 +5,68 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.weather_app.MainViewModel
 import com.example.weather_app.R
+import com.example.weather_app.adapters.WeatherAdapter
+import com.example.weather_app.adapters.WeatherModel
+import com.example.weather_app.databinding.FragmentHoursBinding
+import org.json.JSONArray
+import org.json.JSONObject
 
 class HoursFragment : Fragment() {
 
-
+    private lateinit var binding:FragmentHoursBinding
+    private lateinit var adapter:WeatherAdapter
+    private val model:MainViewModel by activityViewModels()
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_hours, container, false)
+    ): View {
+        binding=FragmentHoursBinding.inflate(inflater,container,false)
+        return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initRcView()
+        model.liveDataCurrent.observe(viewLifecycleOwner){
+            adapter.submitList(getHoursList(it))
+
+        }
+    }
+    private fun initRcView()=with(binding){
+        rcView.layoutManager=LinearLayoutManager(activity)
+        adapter= WeatherAdapter()
+        rcView.adapter=adapter
+
+    }
+//Заполнение маленькой карточки
+    private fun getHoursList(wItem:WeatherModel):List<WeatherModel>{
+        val hoursArray=JSONArray(wItem.hours)
+        val list = ArrayList<WeatherModel>()
+        for(i in 0 until hoursArray.length()) {
+            val item = WeatherModel(
+                wItem.city,
+                (hoursArray[i] as JSONObject).getString("time"),
+                (hoursArray[i] as JSONObject).getJSONObject("condition")
+                    .getString("text"),
+                (hoursArray[i] as JSONObject).getString("temp_c"),
+                maxTemp = "",
+                minTemp = "",
+                (hoursArray[i] as JSONObject).getJSONObject("condition")
+                    .getString("icon"),
+                ""
+
+
+
+
+            )
+            list.add(item)
+        }
+    return list
+    }
     companion object {
 
         @JvmStatic
--- app/src/main/java/com/example/weather_app/fragments/MainFragment.kt
+++ app/src/main/java/com/example/weather_app/fragments/MainFragment.kt
@@ -1,6 +1,8 @@
 package com.example.weather_app.fragments
 
 import android.os.Bundle
+import android.service.voice.VoiceInteractionSession.Request
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -9,11 +11,20 @@ import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
+import androidx.fragment.app.activityViewModels
 import androidx.viewpager2.widget.ViewPager2
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+import com.example.weather_app.MainViewModel
 import com.example.weather_app.adapters.VpAdapte
+import com.example.weather_app.adapters.WeatherModel
 import com.example.weather_app.databinding.FragmentMainBinding
 import com.google.android.material.tabs.TabLayoutMediator
+import com.squareup.picasso.Picasso
+import org.json.JSONObject
+import kotlin.math.log
 
+const val API_KEY="0b6068d8628b40c6b7a130405232212"
 //главный фрагмент
 class MainFragment : Fragment() {
     private val flist= listOf(
@@ -26,6 +37,7 @@ class MainFragment : Fragment() {
     )
     private lateinit var pLauncher:ActivityResultLauncher<String>
     private lateinit var binding: FragmentMainBinding
+    private val model: MainViewModel by activityViewModels()
 
 
 
@@ -44,6 +56,11 @@ class MainFragment : Fragment() {
         permissionListener()
         checkPermission()
         init()
+        updateCurrentCard()
+        requestWeatherData("Krasnoyarsk")
+
+
+
 
     }
     //переключение табло
@@ -55,7 +72,19 @@ class MainFragment : Fragment() {
             tab,pos->tab.text=tList[pos]
         }.attach()
     }
+    //заполнение карточки
+    private fun updateCurrentCard()= with(binding){
+        model.liveDataCurrent.observe(viewLifecycleOwner){
+            val maxMinTemp="${it.maxTemp} C /${it.minTemp} C"
+            tvData.text=it.time
+            tvCity.text=it.city
+            tvcurrentTemp.text=it.currentTemp
+            tvCondition.text=it.condition
+            tvMaxMin.text=maxMinTemp
+            Picasso.get().load("https:"+it.imageUrl).into(imWeather)
 
+        }
+    }
 //даёт ли пользователь разрешение на исп. местоположения
     private fun permissionListener(){
         pLauncher=registerForActivityResult(
@@ -64,6 +93,79 @@ class MainFragment : Fragment() {
             
         }
     }
+    private fun requestWeatherData(city: String){
+        val url="https://api.weatherapi.com/v1/forecast.json?key=" +
+                API_KEY +
+                "&q=" +
+                city +
+                "&days=" +
+                "3" +
+                "&aqi=no&alerts=no"
+    val queue=Volley.newRequestQueue(context)
+    val request=StringRequest(
+        com.android.volley.Request.Method.GET,url,
+        {
+            result->
+            parseWeatherData(result)
+        },
+        {
+            error->
+
+        }
+    )
+    queue.add(request)
+    }
+    //Вытаскивание информации из JSON ФАЙЛА
+    private fun parseWeatherData(result:String){
+        val mainObject=JSONObject(result)
+        val list=parseDays(mainObject)
+        parseCurrentData(mainObject,list[0])
+
+    }
+
+    private fun parseDays(mainObject: JSONObject):List<WeatherModel>{
+        val list=ArrayList<WeatherModel>()
+        val daysArray=mainObject.getJSONObject("forecast")
+            .getJSONArray("forecastday")
+        val name=mainObject.getJSONObject("location").getString("name")
+        for (i in 0 until daysArray.length()) {
+            val day=daysArray[i] as JSONObject
+            val item=WeatherModel(
+                name,
+                day.getString("date"),
+                day.getJSONObject("day").getJSONObject("condition")
+                    .getString("text"), currentTemp = "",
+                day.getJSONObject("day").getString("maxtemp_c"),
+                day.getJSONObject("day").getString("mintemp_c"),
+                day.getJSONObject("day").getJSONObject("condition").getString("icon"),
+                day.getJSONArray("hour").toString()
+
+
+            )
+            list.add(item)
+
+
+        }
+        return list
+    }
+    private fun parseCurrentData(mainObject:JSONObject,weatherItem:WeatherModel){
+
+        val item=WeatherModel(
+            mainObject.getJSONObject("location").getString("name"),
+            mainObject.getJSONObject("current").getString("last_updated"),
+            mainObject.getJSONObject("current").getJSONObject("condition").getString("text"),
+            mainObject.getJSONObject("current").getString("temp_c"),
+            weatherItem.maxTemp,
+            weatherItem.minTemp,
+            mainObject.getJSONObject("current").getJSONObject("condition").getString("icon"),
+            weatherItem.hours
+        )
+        model.liveDataCurrent.value=item
+        Log.d("MyLog","city: ${item.maxTemp}")
+        Log.d("MyLog","city: ${item.minTemp}")
+        Log.d("MyLog","city: ${item.hours}")
+    }
+
     //если нет разрешения то с помощью лаунчера спросить ответ
     private fun checkPermission(){
         if(!isPermissionsGranted(android.Manifest.permission.ACCESS_FINE_LOCATION)){
